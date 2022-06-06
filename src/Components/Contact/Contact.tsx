d6bee0c9d8b1404d7f205bc4af79a3a530484c5f
Oskar Wal <oskar.amadeusz.wal@gmail.com>
2022-06-06 22:16:21 +0200

    Delete Contact

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,31 +1,64 @@
-import {Avatar, Button, ListItem} from "@mui/material";
+import {Avatar, ListItem} from "@mui/material";
 import { FcMoneyTransfer } from "react-icons/fc";
 import {IoTrashBin} from "react-icons/io5";
 import {GrEdit} from "react-icons/gr";
 
-const goToTransaction = () =>
+import React  from "react";
+import {Link} from "react-router-dom";
+import AuthFetchService from "../../Services/AuthFetchService";
+import {toast, ToastContainer} from "react-toastify";
+
+
+const deleteContact = (username : string) =>
 {
-    console.log("Zrob przelew")
+
+    return AuthFetchService.authenticatedApiFetch({
+        url: '/contacts/'+username,
+        method: 'DELETE',
+        additionalHeaders: {},
+        params: JSON.stringify("")
+    });
+
 }
 
-const goToEdit = () =>
-{
-    console.log("Edycja")
-}
-
-const goToDelete = () =>
-{
-    console.log("Usuń")
-}
 
 const Contact = ({ contact } : any) => {
+
+    const onDelete = (username :string, customName :string) => {
+
+        if(window.confirm("Czy napewno chcesz usunąć kontakt "+ customName +" ?"))
+        {
+            deleteContact(username)
+                .then( (data) => {
+                    if( data.status === 404 ){
+                        alert("Nieoczekiwany problem spróbuj ponownie później");
+                    }
+                    else
+                    {
+                        toast("Pomyślnie usunięto kontakt");
+                        setTimeout(function () { (window.location.reload()) }, 1500);
+                    }
+
+
+                })
+        }
+
+    }
+
+
     return(
         <ListItem>
+            <ToastContainer />
             <Avatar/>
             <div className={"contact"}> {contact.customName}
-                <FcMoneyTransfer className={"icon"} style={{cursor: 'pointer'}} onClick={goToTransaction}/>
-                <GrEdit className={"icon"} style={{cursor: 'pointer'}} onClick={goToEdit}/>
-                <IoTrashBin className={"icon"} style={{color:'red', cursor: 'pointer'}} onClick={goToDelete}/>
+
+                <Link to={`/transactions/create` } state={{username:contact.username}}  >
+                    <FcMoneyTransfer className={"icon"} style={{cursor: 'pointer'}} />
+                </Link>
+                <GrEdit className={"icon"} style={{cursor: 'pointer'}} />
+                <IoTrashBin className={"icon"} style={{color:'red', cursor: 'pointer'}} onClick={() => onDelete(contact.username, contact.customName)}/>
+
+
             </div>
         </ListItem>
     )
