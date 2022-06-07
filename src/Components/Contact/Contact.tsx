cd9d0eac3a9461bb13c6a6d3216a955eaaaeb1c5
Oskar Wal <oskar.amadeusz.wal@gmail.com>
2022-06-07 13:34:02 +0200

    Contacts Finished

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,19 +1,26 @@
-import {Avatar, ListItem} from "@mui/material";
+import {Avatar, Button, ListItem, TextField} from "@mui/material";
 import { FcMoneyTransfer } from "react-icons/fc";
 import {IoTrashBin} from "react-icons/io5";
 import {GrEdit} from "react-icons/gr";
 
-import React  from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import AuthFetchService from "../../Services/AuthFetchService";
 import {toast, ToastContainer} from "react-toastify";
 
+const editContact = (username:string, newCustomName:string) => {
+    return AuthFetchService.authenticatedApiFetch({
+        url: `/contacts/${username}`,
+        method: 'PUT',
+        additionalHeaders: {},
+        params: JSON.stringify({username: newCustomName})
+    });
 
-const deleteContact = (username : string) =>
-{
+}
+const deleteContact = (username : string) => {
 
     return AuthFetchService.authenticatedApiFetch({
-        url: '/contacts/'+username,
+        url: `/contacts/${username}`,
         method: 'DELETE',
         additionalHeaders: {},
         params: JSON.stringify("")
@@ -23,6 +30,10 @@ const deleteContact = (username : string) =>
 
 
 const Contact = ({ contact } : any) => {
+
+    const [showEdit, setShowEdit] = useState(false)
+    const [newCustomName, setNewCustomName] = useState('')
+
 
     const onDelete = (username :string, customName :string) => {
 
@@ -39,6 +50,28 @@ const Contact = ({ contact } : any) => {
                         setTimeout(function () { (window.location.reload()) }, 1500);
                     }
 
+                })
+        }
+
+    }
+
+    const onEdit = (e:any) => {
+
+        e.preventDefault()
+
+        if(window.confirm(`Czy napewno chcesz zmienić nazwę kontatu z ${contact.customName} na ${newCustomName} ?`))
+        {
+            editContact(contact.username, newCustomName)
+                .then( (data) => {
+                    console.log(data)
+                    if( data.status === 404 ){
+                        alert("Nieoczekiwany problem spróbuj ponownie później");
+                    }
+                    else
+                    {
+                        toast("Pomyślnie edytowano kontakt");
+                        setTimeout(function () { (window.location.reload()) }, 1500);
+                    }
 
                 })
         }
@@ -55,12 +88,29 @@ const Contact = ({ contact } : any) => {
                 <Link to={`/transactions/create` } state={{username:contact.username}}  >
                     <FcMoneyTransfer className={"icon"} style={{cursor: 'pointer'}} />
                 </Link>
-                <GrEdit className={"icon"} style={{cursor: 'pointer'}} />
+                <GrEdit className={"icon"} style={{cursor: 'pointer'}} onClick={()=>setShowEdit(!showEdit)}/>
                 <IoTrashBin className={"icon"} style={{color:'red', cursor: 'pointer'}} onClick={() => onDelete(contact.username, contact.customName)}/>
 
+                {showEdit && (
+                    <div className={'little-container'}>
+                        <form onSubmit={onEdit}>
+                            <TextField label="Login" variant="outlined" sx={{mb: 1, width:"100%"} }
+                                       value={contact.username}
+                                       disabled={true}/>
+                            <TextField label="Nowa nazwa" variant="outlined" sx={{mb: 1, width:"100%"} }
+                                       value={newCustomName}
+                                       onChange={(e) => setNewCustomName(e.target.value)}
+                            />
+                            <Button type="submit" variant="contained" color="primary" sx={{width: "100%"}} >
+                                Edytuj
+                            </Button>
+                        </form>
+                    </div>
+                )}
 
             </div>
         </ListItem>
+
     )
 
 }
